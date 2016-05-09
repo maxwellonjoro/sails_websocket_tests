9ef13a7a70ba7f45ace9087d13bd17b02b6b6d81
Maxwell Onjoro <maxwell.onjoro@riverbank.co.ke>
2016-05-09 12:35:19 +0300

    Added hook to start the websocket server on SailsJS lift

---
+++ api/hooks/myhooks/index.js
@@ -0,0 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+module.exports = function (sails){
+    return {
+        // Run when sails loads-- be sure and call `next()`.
+        initialize: function (next) {
+            WebSocket.createWSNode();
+            return next();
+        }
+
+  };
+
+}
