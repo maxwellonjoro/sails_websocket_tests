a46ca6ccca93a588555086c859dbe93920a690f3
Maxwell Onjoro <maxwell.onjoro@riverbank.co.ke>
2016-05-09 12:29:06 +0300

    Parsed the input to JSON and added the message number the response string

--- api/services/WebSocket.js
+++ api/services/WebSocket.js
@@ -7,72 +7,12 @@
 var countNoOfPhoneRequests = 0;
 
 module.exports = {
-    
-    createWS:function(){
-        var autobahn = require('autobahn');
-        
-        var connection = new autobahn.Connection({
-            transports: [
-                {
-                   'type': 'websocket',
-                   'url': 'ws://localhost:9999'
-                },
-                {
-                   'type': 'longpoll',
-                   'url': 'http://localhost:1337/lp'
-                }
-            ],
-            realm: 'realm1'
-          });
-
-            connection.onopen = function (session) {
-                
-                console.log("Connection opened");
-
-               // 1) subscribe to a topic
-               function onevent(args) {
-                  console.log("Event:", args[0]);
-                  console.log("Session open.");
-               }
-
-               session.subscribe('echo', onevent);
-
-               // 2) publish an event
-               session.publish('echo', ['Hello, world!']);
-
-               // 3) register a procedure for remoting
-               function add2(args) {
-                  return args[0] + args[1];
-               }
-               session.register('com.myapp.add2', add2);
-
-               // 4) call a remote procedure
-               session.call('com.myapp.add2', [2, 3]).then(
-                  function (res) {
-                     console.log("Result:", res);
-                  }
-               );
-            };
-            connection.onclose = function(session){
-                console.log("Connection closed");
-            };
-
-            connection.open();
-
-            console.log(connection.isOpen);
-            console.log(connection.isConnected);
-            console.log("Finished loading");
-            return;
-
-        },
-        createWSEngineIO: function () {
-            var engine = require('engine.io');
-            var server = engine.listen(9999);
-
-              server.on('connection', function(socket){
-              socket.send('utf 8 string');
-              socket.send(new Buffer([0, 1, 2, 3, 4, 5])); // binary data
-            });
+       testJSON: function () {
+            var orig_json = {};
+            orig_json.max = "Max";
+            orig_json.age = "19";
+            orig_json.online = true;
+            console.log(orig_json);
         },
         createWSNode:function(){
             var WebSocketServer = require('websocket').server;
@@ -82,7 +22,7 @@ module.exports = {
                 // process HTTP request. Since we're writing just WebSockets server
                 // we don't have to implement anything.
             });
-            server.listen(8080, function() { });
+            server.listen(9999, function() { });
 
             // create the server
             wsServer = new WebSocketServer({
@@ -99,11 +39,12 @@ module.exports = {
                 connection.on('message', function(message) {
                     if (message.type === 'utf8') {
                         // process WebSocket message
+                        var json_obj = JSON.parse(message.utf8Data);
+                        console.log(json_obj);
                         countNoOfPhoneRequests++;
-                        console.log(countNoOfPhoneRequests+"Message: "+message.utf8Data);
-                        message.utf8Data.msgNo = countNoOfPhoneRequests;
-                        console.log(message.utf8Data);
-                        connection.send(message.utf8Data);
+                        json_obj.msgNo = countNoOfPhoneRequests;
+                        console.log(json_obj);
+                        connection.send(JSON.stringify(json_obj));
                     }
                 });
 
